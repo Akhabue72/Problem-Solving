d3dc6d874e0f06270850b4de66e0f04d9dd492b6
Akhabue72 <abuakioya99@gmail.com>
2026-01-27 12:27:36 +0000

    partway through task 2

---
+++ Lesson 2.py
@@ -0,0 +1,47 @@
+# 1,2,3
+"""
+# age = int(input("What is your age? "))
+if age <= 0:
+     print("wow, people start using computers early")
+if age == 19:
+     print("Hey! We're the same age!")
+day = int(input("What day is it? "))
+month = int(input("What month is it? "))
+if (day == 1 ) and (month == 4):
+     print("It's April fools day")
+"""
+# 4
+"""
+age = int(input("What is your age? "))
+if age >= 18:
+    print(f"You are old enough to buy alcohol")
+else:
+    print(f"You are too young to buy alcohol")
+"""
+
+#5,6
+"""
+speed = int(input("How fast are you going?"))
+speedLimit = 50
+cap = speed - speedLimit
+if cap <= 10 and speed >= speedLimit:
+    print(f"You are {cap} over the speed limit. Slow down")
+elif cap >10:
+    print(f"You're speeding! Ticket for you")
+else:
+    print("Please continue to drive safely")
+"""
+# 7
+day = int(input("What day is it? "))
+month = int(input("What month is it? "))
+if (day == 1 ) and (month == 1):
+     print("It's New Years Day")
+elif (day == 1 ) and (month == 4):
+     print("It's April fools day")
+elif (day == 18 ) and (month == 2):
+     print("It's my birthday")
+elif (day == 4 ) and (month == 5):
+     print("It's Star Wars Day")
+else:
+    print("Mid day icl")
+
