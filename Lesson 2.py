6a56476bd22cf0b393f39ad25cd546b46c07c10b
Akhabue72 <abuakioya99@gmail.com>
2026-01-30 14:41:08 +0000

    Not much

--- Lesson 2.py
+++ Lesson 2.py
@@ -1,65 +1,67 @@
-# 1,2,3
-"""
+# # 1,2,3
+#
+# agE = int(input("What is your age? "))
+# if agE <= 0:
+#      print("wow, people start using computers early")
+# if agE == 19:
+#      print("Hey! We're the same age!")
+# day = int(input("What day is it? "))
+# month = int(input("What month is it? "))
+# if (day == 1 ) and (month == 4):
+#      print("It's April fools day")
+#
+# # 4
+#
 # age = int(input("What is your age? "))
-if age <= 0:
-     print("wow, people start using computers early")
-if age == 19:
-     print("Hey! We're the same age!")
-day = int(input("What day is it? "))
-month = int(input("What month is it? "))
-if (day == 1 ) and (month == 4):
-     print("It's April fools day")
-"""
-# 4
-"""
-age = int(input("What is your age? "))
-if age >= 18:
-    print(f"You are old enough to buy alcohol")
-else:
-    print(f"You are too young to buy alcohol")
-"""
+# if age >= 18:
+#     print(f"You are old enough to buy alcohol")
+# else:
+#     print(f"You are too young to buy alcohol")
+#
+#
+# #5,6
+#
+# speed = int(input("How fast are you going?"))
+# speedLimit = 50
+# cap = speed - speedLimit
+# if cap <= 10 and speed >= speedLimit:
+#     print(f"You are {cap} over the speed limit. Slow down")
+# elif cap >10:
+#     print(f"You're speeding! Ticket for you")
+# else:
+#     print("Please continue to drive safely")
 
-#5,6
-"""
-speed = int(input("How fast are you going?"))
-speedLimit = 50
-cap = speed - speedLimit
-if cap <= 10 and speed >= speedLimit:
-    print(f"You are {cap} over the speed limit. Slow down")
-elif cap >10:
-    print(f"You're speeding! Ticket for you")
-else:
-    print("Please continue to drive safely")
-"""
-# 7
-"""
-day = int(input("What day is it? "))
-month = int(input("What month is it? "))
-if (day == 1 ) and (month == 1):
-     print("It's New Years Day")
-elif (day == 1 ) and (month == 4):
-     print("It's April fools day")
-elif (day == 18 ) and (month == 2):
-     print("It's my birthday")
-elif (day == 4 ) and (month == 5):
-     print("It's Star Wars Day")
-else:
-    print("Mid day icl")
-"""
-# 8
-age = int(input("What is your age? "))
-price = float(4)
-
-if age <= 4:
-     print(f"You pay nothing!")
-elif age <= 16 and age > 4:
-    teen_price = price // 2
-    print (f"You pay £{teen_price}")
-elif age >= 65:
-    olie_price = price * 2/3
-    print (f"You pay £{olie_price}")
-else:
-    print (f"You pay £{price}")
+# # 7
+#
+# day = int(input("What day is it? "))
+# month = int(input("What month is it? "))
+#
+# match day , month:
+#     case s if day == 1 & month == 4:
+#         print("It's april fools day")
+#     case s if day == 1 & month == 1:
+#         print("It's New Year Day")
+#     case s if day == 18 & month == 2:
+#         print("Its My birthday!")
+#     case s if day == 1 & month == 4:
+#         print("It's Star Wars Day")
+#     case _:
+#         print("Mid day icl")
+#
+# # 8
+# age = int(input("What is your age? "))
+# price = float(4)
+#
+# if age <= 4:
+#      print(f"You pay nothing!")
+# elif age <= 16 and age > 4:
+#     teen_price = price // 2
+#     print (f"You pay £{teen_price}")
+# elif age >= 65:
+#     olie_price = price * 2/3
+#     print (f"You pay £{olie_price}")
+# else:
+#     print (f"You pay £{price}")
 
 
 
